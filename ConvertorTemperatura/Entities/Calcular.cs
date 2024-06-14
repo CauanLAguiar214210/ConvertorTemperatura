1a89cfaf18582f2ab409d5ea23c74b529a7ce2d4
CauanLAguiar <132419025+CauanLAguiar214210@users.noreply.github.com>
2024-06-13 22:16:26 -0300

    Commit1

---
+++ ConvertorTemperatura/Entities/Calcular.cs
@@ -0,0 +1,23 @@
+﻿namespace ConvertorTemperatura.Entities
+{
+    internal class Calcular
+    {
+        public double Celcius { get; set; }
+
+        public Calcular(double celcius)
+        {
+            Celcius = celcius;
+        }
+
+        public static double Fahrenheit(double celcius)
+        {
+            return 9.0 * celcius / 5.0 + 32.0;
+        }
+
+        public static double Kelvin(double celcius)
+        {
+            return celcius + 273.15;
+        }
+
+    }
+}
---
+++ ConvertorTemperatura/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ConvertorTemperatura.Entities;
+using System.Globalization;
+
+namespace ConvertorTemperatura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celcius;
+            char repetir;
+
+            do
+            {
+                Console.Write("Informe a Temperatura em °Celcius para conversão: ");
+                celcius = double.Parse(Console.ReadLine());
+
+                double fahrenheit = Calcular.Fahrenheit(celcius);
+                double kelvin = Calcular.Kelvin(celcius);
+
+                Console.WriteLine($"\nTemperatura atual: {celcius}°C \nEquivalente em Fahrenheit: {fahrenheit}°F \nEquivalente em Kelvin {kelvin.ToString("F2")}K");
+
+                Console.WriteLine("\nDeseja repetir (s/n):");
+                repetir = char.Parse(Console.ReadLine());
+            } while (repetir == 's');
+        }
+    }
+}
